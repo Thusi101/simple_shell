9e75cf967389941db6d5bb530d1fd2fab94dd912
Thusi101 <moraseroka10@gmail.com>
2024-01-11 10:53:25 -0800

    just moved to main branch

---
+++ file_loc.c
@@ -0,0 +1,100 @@
+#include "main.h"
+
+/**
+ * get_file_path - shows full path of file
+ * startswithfwdslash - Checks if the file starts with "/"
+ * @str: The filename to be examined
+ * @file_name: Argument name
+ * @path: Full path variable
+ *
+ * Return: 0 if yes & 1 if No
+ */
+
+int startswithfwdslash(const char *str)
+{
+	if (str != NULL || str[0] == '/')
+		return (1);
+	return (0);
+}
+/**
+ * get_file_loc - Gets exec path file
+ * @path: Path variable
+ * @file_name: The exec file
+ * array: Argument array
+ *
+ * Return: Full path to exec files
+ */
+char *get_file_loc(char *file_name, char *path)
+{
+	char *path_copy, *token;
+	struct stat file_path;
+	char *path_buffer = NULL;
+
+	path_copy = strdup(path);
+	token = strtok(path_copy, ":");
+
+	while (token)
+	{
+		if (path_buffer)
+		{
+			free(path_buffer);
+			path_buffer = NULL;
+		}
+		path_buffer = malloc(strlen(token) + strlen(file_name) + 2);
+		if (!path_buffer)
+		{
+			perror("Error: Malloc failed");
+			exit(EXIT_FAILURE);
+		}
+		strcpy(path_buffer, token);
+		strcat(path_buffer, "/");
+		strcat(path_buffer, file_name);
+		strcat(path_buffer, "\0");
+
+		if (stat(path_buffer, &file_path) == 0 && access(path_buffer, X_OK) == 0)
+		{
+			free(path_copy);
+				return (path_buffer);
+		}
+		token = strtok(NULL, ":");
+	}
+	free(path_copy);
+	if (path_buffer)
+		free(path_buffer);
+	return (NULL);
+}
+
+/**
+ * get_file_path - Obtains full path of file
+ * @file_name: Argument name
+ *
+ * Return: Full path Argument of file
+ */
+
+char *get_file_path(char *file_name)
+{
+	char *path = getenv("PATH");
+	char *full_path;
+
+	if (startswithfwdslash(file_name) &&
+			access(file_name, X_OK) == 0)
+		return (strdup(file_name));
+
+	if (!path)
+	{
+		perror("Path not found");
+		return (NULL);
+	}
+
+	full_path = get_file_loc(path, file_name);
+	if (full_path == NULL)
+	{
+		write(2, file_name, strlen(file_name));
+		write(2, ": command not found\n", 19);
+		return (NULL);
+	}
+	path = getenv("PATH");
+	printf("PATH: %s\n", path);
+
+	return (full_path);
+}
---
+++ main.c
@@ -0,0 +1,68 @@
+#include "main.h"
+
+/**
+ * main - To Getline function
+ * @path: Full path variable
+ * @ac:Argument count
+ * @argv:Array of argument values
+ *
+ * Return:0 on success
+ */
+
+int main(int ac, char **argv)
+{
+        (void)ac,(void)argv;
+        char *buf = NULL, *token;
+        size_t count = 0;
+        ssize_t nread;
+        pid_t child_pid;
+        int i;
+        int status;
+        char **array;
+	char *path;
+
+        while(1)
+        {
+		if (isatty(STDIN_FILENO))
+                write(STDOUT_FILENO, "2CShell$ ", 9);
+                
+                nread = getline(&buf, &count, stdin);
+                if(nread == -1)
+                {
+                        perror("Exiting the shell");
+                        exit(51);
+                }
+                token = strtok(buf," \n");
+                array = malloc(sizeof(char*) * 1024);
+                i = 0;
+                while (token)
+                {
+                        array[i] = token;
+                        token = strtok(NULL," \n");
+			i++;
+		}
+		array[i] = NULL;
+		path = get_file_path(array[0]);
+		child_pid = fork();
+		if(child_pid == -1)
+		{
+			perror("Failed to create.");
+			exit(51);
+		}
+		if(child_pid == 0)
+		{
+			if(execve(path, array, NULL) == -1)
+			{
+				perror("Failed to execute");
+				exit(52);
+			}
+		}
+		else
+		{
+			wait(&status);
+		}
+	}
+	free(buf);
+	free(path);
+        return(0);
+}
