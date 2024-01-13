f71e26b954b33990be3dfa446a07c4a958d8a6b8
Thusi101 <moraseroka10@gmail.com>
2024-01-12 17:24:50 -0800

    final

--- bad.c
+++ bad.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ *_putsfd - prints an input string
+ * @str: the string to be printed
+ * @fd: the filedescriptor to write to
+ *
+ * Return: the number of chars put
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
+
+/**
  *_eputs - prints an input string
  * @str: the string to be printed
  *
@@ -62,24 +82,4 @@ int _putfd(char c, int fd)
 	if (c != BUF_FLUSH)
 		buf[i++] = c;
 	return (1);
-}
-
-/**
- *_putsfd - prints an input string
- * @str: the string to be printed
- * @fd: the filedescriptor to write to
- *
- * Return: the number of chars put
- */
-int _putsfd(char *str, int fd)
-{
-	int i = 0;
-
-	if (!str)
-		return (0);
-	while (*str)
-	{
-		i += _putfd(*str++, fd);
-	}
-	return (i);
 }
